557a852c7f5a3a4cea52ee65f8b213af64f5e8a3
Maxime Bret <maxime.bret.ext@boursorama.fr>
2017-02-28 12:57:47 +0100

    no message

--- index.js
+++ index.js
@@ -18,10 +18,12 @@ class MyHook {
      * @param {Object} chewie - Chewie instance
      * @param {Object} userConfig - This is the config set by user for your hook. You can use it with your own config.
      * @param {Object} helper - An helper that help you dealing with such things as your hook storage, ...
-     *                 You are never obliged to use the helper as all it does is using chewie instance and provide more convenient methods for hooks.
-     * @param {Object} helper.logger - A convenient logger you may use to display information. logger provide .info, .verbose, .waring, .error.
-     *                 helper.logger is not really meant to be used as debug tool (although you can), you can use debug() for that. The logs will be displayed to
-     *                 user depending of its config (info, verbose, warning, error)
+     *                 You are never obliged to use the helper as all it does is using chewie instance
+     *                 and provide more convenient methods for hooks.
+     * @param {Object} helper.logger - A convenient logger you may use to display information.
+     *                 logger provide .info, .verbose, .waring, .error.
+     *                 helper.logger is not really meant to be used as debug tool (although you can), you can use debug() for that.
+     *                 The logs will be displayed to user depending of its config (info, verbose, warning, error)
      */
     constructor(chewie, userConfig, helper) {
         this.chewie = chewie;
